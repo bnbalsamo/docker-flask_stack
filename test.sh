29ab69cbbec1bc98d33ef0f33f8093e624097937
brian <balsamo@uchicago.edu>
2017-10-03 11:39:54 -0500

    Tying django demo to tests, adding a link to the README

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Building Template..."
 docker build . -t flask_stack_test && \
-echo "Cloning Demo..." && \
+echo "Cloning Flask Demo..." && \
 git clone https://github.com/bnbalsamo/flask_stack_minimal_demo.git || exit 1 
 cd flask_stack_minimal_demo || exit 1 
 echo "Altering demo Dockerfile"
@@ -21,6 +21,22 @@ response=$(curl --silent localhost:5000)
 echo "$response"
 if [[ $response != "Hello, World"'!' ]]; then echo "$response" && exit 1; fi || exit 1
 echo "All good!"
+cd ..
+echo "Cloning Django Demo..." && \
+git clone https://github.com/bnbalsamo/flask_stack_minimal_django_demo.git || exit 1
+cd flask_stack_minimal_django_demo || exit 1
+echo "Altering demo Dockerfile"
+cat Dockerfile | sed 's/bnbalsamo\/flask_stack/flask_stack_test/g' > Dockerfile.test
+echo "Building Demo..." 
+docker build . --file Dockerfile.test -t my_demo_test_django || exit 1 
+echo "Running Demo..."
+docker run -d -p 5001:80 my_demo_test_django || exit 1
+sleep 3
+echo "It's running!" 
+echo "curling for status code..." 
+response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:5001) 
+echo $response 
+if [[ $response -ne 200 ]]; then echo "$response" && exit 1; fi || exit 1 
 echo "Here's some stats"
 docker images
 exit 0
